d46f254a8a4844a07ad594030f6c6bab0d272d25
avanishgupta07 <avanishguptaaza@gmail.com>
2024-07-14 20:42:59 +0530

    first commit

---
+++ server/database/db.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const DBConnection = async () => {
+    const USERNAME = process.env.DB_USERNAME;
+    const PASSWORD = process.env.DB_PASSWORD;
+
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.log('Error while connecting with the database ', error.message);
+    }
+}
+
+export default DBConnection;
